e588374920459f5917a522884b2ee0f5f48d1e0c
愚千一 <52897651+yuqianyi1001@users.noreply.github.com>
2023-08-03 21:42:59 +0800

    Update install.sh

--- azure/install.sh
+++ azure/install.sh
@@ -4,4 +4,6 @@ export VPN_IPSEC_PSK=$1
 export VPN_USER=$2
 export VPN_PASSWORD=$3
 
+sudo apt-get update
+
 wget -t 3 -T 30 -nv -O vpn.sh https://raw.githubusercontent.com/hwdsl2/setup-ipsec-vpn/master/vpnsetup.sh && sh vpn.sh
